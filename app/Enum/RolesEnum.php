3194b8f33b79733df289396ab772e61b7c303407
1JP <joaopedrogs1@outlook.com>
2024-10-27 12:29:05 -0300

    insert roles

---
+++ app/Enum/RolesEnum.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Enum;
+
+class RolesEnum
+{
+
+    public static $data = [
+        [
+            'name' => 'Admin',
+            'guard_name' => 'web',
+        ],
+        [
+            'name' => 'Membros',
+            'guard_name' => 'web',
+        ],
+        [
+            'name' => 'Usuario',
+            'guard_name' => 'web',
+        ],
+        [
+            'name' => 'Cliente',
+            'guard_name' => 'web',
+        ],
+        [
+            'name' => 'Root',
+            'guard_name' => 'web',
+        ],
+    ];
+
+    public static function all() {
+        return self::$data;
+    }
+}
