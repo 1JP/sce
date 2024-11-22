b16e1e409683fe528af860e11fcbabf62c6b40c9
1JP <joaopedrogs1@outlook.com>
2024-11-22 20:04:51 -0300

    policy and test policy deslink

--- app/Policies/DeslinkPolicy.php
+++ app/Policies/DeslinkPolicy.php
@@ -3,10 +3,10 @@
 namespace App\Policies;
 
 use Illuminate\Auth\Access\Response;
-use App\Models\Comment;
+use App\Models\Deslink;
 use App\Models\User;
 
-class CommentPolicy
+class DeslinkPolicy
 {
     /**
      * Determine whether the user can view any models.
@@ -19,7 +19,7 @@ class CommentPolicy
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, Comment $comment): bool
+    public function view(User $user, Deslink $deslink): bool
     {
         return true;
     }
@@ -35,32 +35,32 @@ class CommentPolicy
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, Comment $comment): bool
+    public function update(User $user, Deslink $deslink): bool
     {
-        return $user->hasRole(['Root']) || $user->id == $comment->user_id;
+        return true;
     }
 
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, Comment $comment): bool
+    public function delete(User $user, Deslink $deslink): bool
     {
-        return $user->hasRole(['Root']) || $user->id == $comment->user_id;
+        return $user->id == $deslink->user_id;
     }
 
     /**
      * Determine whether the user can restore the model.
      */
-    public function restore(User $user, Comment $comment): bool
+    public function restore(User $user, Deslink $deslink): bool
     {
-        return $user->hasRole(['Root']) || $user->id == $comment->user_id;
+        return $user->id == $deslink->user_id;
     }
 
     /**
      * Determine whether the user can permanently delete the model.
      */
-    public function forceDelete(User $user, Comment $comment): bool
+    public function forceDelete(User $user, Deslink $deslink): bool
     {
-        return $user->hasRole(['Root']) || $user->id == $comment->user_id;
+        return $user->id == $deslink->user_id;
     }
 }
