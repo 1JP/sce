5984e214d2cfd2c29de6d4ef5783ac37dfcebd1d
1JP <joaopedrogs1@outlook.com>
2025-01-23 20:48:12 -0300

    new components

---
+++ resources/js/components/Input.vue
@@ -0,0 +1,82 @@
+<template>
+    <input class="form-control" :class="classInput" :value="value" @blur="handleBlur($event)" @input="handleInput($event)" :required="required" :type="inputType" :placeholder="placeholder" :name="nameId" :maxlength="maxLength" :disabled="isDisabled">
+</template>
+
+<script>
+    export default {
+        emits: ['input', 'blur'],
+        props: {
+            showLabel: {
+                required: false,
+                type: Boolean,
+                default: true
+            },
+            iconInput: {
+                required: false,
+                type: String,
+                default: ''
+            },
+            nameId: {
+                required: false,
+                type: String,
+                default: ''
+            },
+            placeholder: {
+                required: false,
+                type: String,
+                default: ''
+            },
+            instruction: {
+                required: false,
+                type: String,
+                default: ''
+            },
+            widths: {
+                required: false,
+                default: '100%',
+                type: String
+            },
+            mode: {
+                required: false,
+                type: String,
+                default: 'none'
+            },
+            inputType: {
+                required: false,
+                type: String,
+                default: 'text'
+            },
+            value: {
+                default: ''
+            },
+            maxLength: {
+                required: false,
+                default: ''
+            },
+            isDisabled: {
+                required: false,
+                default: false
+            },
+            required: {
+                required: false,
+                default: false
+            },
+            classInput: {
+                required: false,
+                default: ''
+            }
+        },
+        data () {
+            return {
+            }
+        },
+        methods: {
+            handleInput (e) {
+                this.$emit('input', e)
+            },
+            handleBlur (e) {
+                this.$emit('blur', e)
+            }
+        },
+    }
+</script>
---
+++ resources/js/components/Select.vue
@@ -0,0 +1,28 @@
+<template>
+    <select class="form-control" :id="id" :required="isRequired">
+        <option v-for="option,index in options" :key="index">{{ option }}</option>
+    </select>
+</template>
+
+<script>
+    export default {
+        emits: ['onChanged'],
+        props: {
+            options: {
+                type: Array,
+                default: () => []
+            },
+            isRequired: {
+                type: Boolean,
+                default: false
+            }
+
+        },
+        methods: {
+            optionsSelected(value){
+                this.select.push(value+';');
+                this.$emit('onChanged', this.select)
+            }
+        },
+    }
+</script>
