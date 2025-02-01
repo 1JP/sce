d51ab0d3072d20264b2df383714b9b0d15afeb00
1JP <joaopedrogs1@outlook.com>
2025-02-01 20:43:43 -0300

    lista category

---
+++ app/Http/Resources/CategoryResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CategoryResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        $types = '';
+        foreach ($this->categoryTypes as $categoryTypes) {
+            $types .= $categoryTypes->name . ',';
+        }
+        $types = rtrim($types, ',');
+
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'active' => $this->active,
+            'category_types' => $this->categoryTypes,
+            'types' => $types,
+            'posts' => 0,
+        ];
+    }
+}
--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -10,78 +10,10 @@
 <div class="container-fluid py-4">
     <div class="row">
         <div class="col-12">
-            <div class="card mb-4">
-                <div class="card-header pb-0">
-                    <div class="row">
-                        <div class="col-lg-2">
-                            <h6>Categorias</h6>
-                        </div>
-                        <div class="col-lg-1 col-lg-3">
-                            <admin-filter-select
-                                :name="'Classificação Indicativa'"
-                                :options="[]"
-                            ></admin-filter-select>
-                        </div>
-                        <div class="col-lg-1 col-lg-2">
-                            <admin-filter-input
-                                :name="'Categoria...'"
-                                :type="'text'"
-                                :icon="'fa fa-search'"
-                            ></admin-filter-input>
-                        </div>
-                        <div class="col-lg-1 col-lg-2">
-                            <admin-filter-select
-                                :name="'Status'"
-                                :options="[]"
-                            ></admin-filter-select>
-                        </div>
-                        <div class="col-lg-3 d-flex justify-content-end">
-                            <button type="button" class="btn bg-gradient-primary" data-bs-toggle="modal" data-bs-target="#createCategoryModal">
-                                Cadastrar
-                            </button>
-                        </div>
-                    </div>
-                </div>
-                <div class="card-body px-0 pt-0 pb-2">
-                    <admin-table>
-                        <template v-slot:thead>
-                            @foreach ($ths as $th)
-                                <admin-thead
-                                    :class='@json($th['class'])'
-                                    :name='@json($th['name'])'
-                                ></admin-thead>
-                            @endforeach
-                        </template>
-                        <template v-slot:tbody>
-                            
-                            <admin-tr>
-                                <component-td>
-                                    <div class="d-flex px-2 py-1">
-                                        <div class="d-flex flex-column justify-content-center">
-                                            <h6 class="mb-0 text-sm">John Michael</h6>
-                                        </div>
-                                    </div>
-                                </component-td>
-                                <component-td>
-                                    <h6 class="mb-0 text-sm">Manager</h6>
-                                </component-td>
-                                <component-td :class="'align-middle text-center text-sm'">
-                                    <span class="text-secondary text-xs font-weight-bold">40</span>
-                                </component-td>
-                                <component-td :class="'align-middle text-center'">
-                                    <component-span-status :class="'bg-gradient-success'">Online</component-span-status>
-                                </component-td>
-                                <component-td>
-                                    <component-dropdown :name="'teste'">
-                                        <component-dropdown-item name="Editar" route="#"></component-dropdown-item>
-                                        <component-dropdown-item name="Excluir" target="#destoryCategory"></component-dropdown-item>
-                                    </component-dropdown>
-                                </component-td>
-                            </admin-tr>
-                        </template>
-                    </admin-table>
-                </div>
-            </div>
+            <admin-category-index
+                :types='@json($types)'
+                :ths='@json($ths)'
+            />
         </div>
     </div>
 </div>
