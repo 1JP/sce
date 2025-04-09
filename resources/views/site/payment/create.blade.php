7dea697ca01a7eabc63e53e0f174496986f8c36c
1JP <joaopedrogs1@outlook.com>
2025-04-08 23:04:03 -0300

    use new payment component

--- resources/views/site/payment/create.blade.php
+++ resources/views/site/payment/create.blade.php
@@ -7,6 +7,9 @@
         <div class="col-lg-6 col-10 py-2 py-lg-0 order-2 order-md-1">
           <site-create-payment/>
         </div>
+        <div class="col-lg-4 col-10 order-1 order-md-2">
+          <site-summary-payment/>
+        </div>
       </div>
     </div>
 </div>
