d6bd2fb7f92027e1a61c607b144b66c7105aafa3
1JP <joaopedrogs1@outlook.com>
2026-09-10 18:37:37 -0300

    refoctory ReportController

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -13,54 +13,20 @@ class ReportController extends Controller
     public function generalReport(Post $post)
     {
         $post = $post->load('category', 'indicative_rating', 'comments', 'links', 'deslinks');
-        $totalReactions = $post->links->count() + $post->deslinks->count();
         
         $post->likes_total = $post->links->count();
         $post->dislikes_total = $post->deslinks->count();
-        $post->likes_percentage = $totalReactions
-            ? round(($post->links->count() / $totalReactions) * 100)
-            : 0;
-        $post->dislikes_percentage = $totalReactions
-            ? round(($post->deslinks->count() / $totalReactions) * 100)
-            : 0;
-
+        $post->likes_percentage = $this->likes_percentage($post);
+        $post->dislikes_percentage = $this->dislikes_percentage($post);
         $totalComments = $post->comments->count();
-
         $post->total_comments = $totalComments;
-        $post->positive_comments_percentage = $totalComments
-            ? round(($post->comments->where('sentiment', 'positive')->count() / $totalComments) * 100)
-            : 0;
-        $post->negative_comments_percentage = $totalComments
-            ? round(($post->comments->where('sentiment', 'negative')->count() / $totalComments) * 100)
-            : 0;
-        $post->neutral_comments_percentage = $totalComments
-            ? round(($post->comments->where('sentiment', 'neutral')->count() / $totalComments) * 100)
-            : 0;
-
-        $post->most_commented_comments = $post->comments()
-            ->whereNull('comment_id')
-            ->withCount(['links', 'deslinks', 'comments'])
-            ->orderByDesc('comments_count')
-            ->take(10)
-            ->get();
-
-        $post->positive_comments = $post->comments()->where('sentiment', 'positive')
-            ->withCount(['links', 'deslinks', 'comments'])
-            ->orderByDesc('comments_count')
-            ->take(10)
-            ->get();
-
-        $post->negative_comments = $post->comments()->where('sentiment', 'negative')
-            ->withCount(['links', 'deslinks', 'comments'])
-            ->orderByDesc('comments_count')
-            ->take(10)
-            ->get();
-
-        $post->neutral_comments = $post->comments()->where('sentiment', 'neutral')
-            ->withCount(['links', 'deslinks', 'comments'])
-            ->orderByDesc('comments_count')
-            ->take(10)
-            ->get();
+        $post->positive_comments_percentage = $this->positive_comments_percentage($post);
+        $post->negative_comments_percentage = $this->negative_comments_percentage($post);
+        $post->neutral_comments_percentage = $this->neutral_comments_percentage($post);
+        $post->most_commented_comments = $this->most_commented_comments($post);
+        $post->positive_comments = $this->positive_comments($post);
+        $post->negative_comments = $this->negative_comments($post);
+        $post->neutral_comments = $this->neutral_comments($post);
 
         return view('report.general', compact('post'));
     }
@@ -68,8 +34,124 @@ class ReportController extends Controller
     /**
      * Display a comment report of the resource.
      */
-    public function commentReport()
+    public function commentReport(Post $post)
     {
-        return view('report.comment');
+        $post = $post->load('category', 'indicative_rating', 'comments', 'links', 'deslinks');
+
+        $post->likes_percentage = $this->likes_percentage($post);
+        $post->dislikes_percentage = $this->dislikes_percentage($post);
+        $post->positive_comments_percentage = $this->positive_comments_percentage($post);
+        $post->negative_comments_percentage = $this->negative_comments_percentage($post);
+        $post->neutral_comments_percentage = $this->neutral_comments_percentage($post);
+        $post->most_commented_comments = $this->most_commented_comments($post);
+        $post->positive_comments = $this->positive_comments($post);
+        $post->negative_comments = $this->negative_comments($post);
+        $post->neutral_comments = $this->neutral_comments($post);
+
+        return view('report.comment', compact('post'));
+    }
+
+    /**
+     * Calculate the percentage of likes for a post.
+     */
+    private function likes_percentage($post)
+    {
+        $totalReactions = $post->links->count() + $post->deslinks->count();
+        return $totalReactions
+            ? round(($post->links->count() / $totalReactions) * 100)
+            : 0;
+    }
+
+    /**
+     * Calculate the percentage of dislikes for a post.
+     */
+    private function dislikes_percentage($post)
+    {
+        $totalReactions = $post->links->count() + $post->deslinks->count();
+        return $totalReactions
+            ? round(($post->deslinks->count() / $totalReactions) * 100)
+            : 0;
+    }
+
+    /**
+     * Calculate the percentage of positive comments for a post.
+     */
+    private function positive_comments_percentage($post)
+    {
+        $totalReactions = $post->links->count() + $post->deslinks->count() + $post->comments->count();
+        return $totalReactions
+            ? round(($post->comments->where('sentiment', 'positive')->count() / $totalReactions) * 100)
+            : 0;
+    }
+
+    /**
+     * Calculate the percentage of negative comments for a post.
+     */
+    private function negative_comments_percentage($post)
+    {
+        $totalReactions = $post->links->count() + $post->deslinks->count() + $post->comments->count();
+        return $totalReactions
+            ? round(($post->comments->where('sentiment', 'negative')->count() / $totalReactions) * 100)
+            : 0;
+    }
+
+    /**
+     * Calculate the percentage of neutral comments for a post.
+     */
+    private function neutral_comments_percentage($post)
+    {
+        $totalReactions = $post->links->count() + $post->deslinks->count() + $post->comments->count();
+        return $totalReactions
+            ? round(($post->comments->where('sentiment', 'neutral')->count() / $totalReactions) * 100)
+            : 0;
+    }
+
+    /**
+     * Get the most commented comments for a post.
+     */
+    private function most_commented_comments($post)
+    {
+        return $post->comments()
+            ->whereNull('comment_id')
+            ->withCount(['links', 'deslinks', 'comments'])
+            ->orderByDesc('comments_count')
+            ->take(10)
+            ->get();
+    }
+
+    /**
+     * Get the positive comments for a post.
+     */
+    private function positive_comments($post)
+    {
+        return $post->comments()->where('sentiment', 'positive')
+            ->withCount(['links', 'deslinks', 'comments'])
+            ->orderByDesc('comments_count')
+            ->take(10)
+            ->get();
+    }
+
+    /**
+     * Get the negative comments for a post.
+     */
+    private function negative_comments($post)
+    {
+        return $post->comments()->where('sentiment', 'negative')
+            ->withCount(['links', 'deslinks', 'comments'])
+            ->orderByDesc('comments_count')
+            ->take(10)
+            ->get();
+    }
+
+    /**
+     * Get the neutral comments for a post.
+     */
+    private function neutral_comments($post)
+    {
+        return $post->comments()->where('sentiment', 'neutral')
+            ->withCount(['links', 'deslinks', 'comments'])
+            ->orderByDesc('comments_count')
+            ->take(10)
+            ->get();
     }
 }
