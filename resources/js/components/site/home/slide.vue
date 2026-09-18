df5db1ae3fe5637fddd8e83a5e1a28b2627c85e3
1JP <joaopedrogs1@outlook.com>
2026-09-18 16:21:51 -0300

    fix position

--- resources/js/components/site/home/slide.vue
+++ resources/js/components/site/home/slide.vue
@@ -51,10 +51,10 @@
             listPosts(){
                 axios.get(route('api.posts.top'))
                     .then((response) => {
+                        this.allPosts = response.data.data;
                         this.$nextTick(() => {
                             this.initSlider();
                         });
-                        this.allPosts = response.data.data;
                     })
             },
             initSlider() {
